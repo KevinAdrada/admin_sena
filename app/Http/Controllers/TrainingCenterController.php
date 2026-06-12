3a3b964600a40a3b46aebc979236fa1dbf682106
KevinAdrada <kevinsantiagoadrada89@gmail.com>
2026-06-12 11:40:25 -0500

    frms area, training_center, computer

--- app/Http/Controllers/TrainingCenterController.php
+++ app/Http/Controllers/TrainingCenterController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Training_center; 
 
 class TrainingCenterController extends Controller
 {
-    //
+    public function create(){
+        return view('training_center.create'); 
+    }
+
+    public function store(Request $request){
+        $training_center = new Training_center();
+        $training_center->name = $request->name;
+        $training_center->location = $request->location;
+        $training_center->save();
+
+        return $training_center;
+    }
 }
---
+++ resources/views/Computer/create.blade.php
@@ -0,0 +1,28 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="{{route('computer.store')}}" method="POST" enctype="multipart/form-data">
+
+    @csrf
+
+    <label>
+        Número:
+        <br>
+        <input type="number" name="number">
+    </label>
+    <br>
+    <label>
+        Marca:
+        <br>
+        <input type="text" name="brand">
+    </label>
+    <br>
+
+    <button type="submit">Enviar Formulario</button>
+    </form>
+</body>
+</html>
